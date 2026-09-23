662e5a1e2d290c2243f2924e92ad3be55a2c40b0
Ruzica <ruzica.elf@gmail.com>
2026-09-23 15:34:39 +0200

    Created products and displayed them on homepage

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,3 +1,36 @@
-export default function HomePage(){
-  return <div>Homepage</div>;
+import { getProducts } from "../data/products";
+
+export default function HomePage() {
+  const products = getProducts();
+
+  return (
+    <div className="page">
+      <div className="home-hero">
+        <h1 className="Home-title">Welcome to ShopHall</h1>
+
+        <p className="home-subtitle">
+          Discover amazing products at great prices
+        </p>
+
+        <div className="product-grid">
+          {products.map((product) => {
+            return (
+              <div className="product-card" key={product.id}>
+                <img
+                  src={product.image}
+                  className="product-image-card"
+                />
+
+                <div>
+                  <h2>{product.name}</h2>
+                  <p>{product.price}</p>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+
+      </div>
+    </div>
+  );
 }
